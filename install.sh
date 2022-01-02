6ffb1dcd4408ef8b9098d621752204cfc31a02e2
Ben Baryo <ben.baryo@gmail.com>
2022-01-02 11:56:22 +0200

    Loop over tools to install

--- install.sh
+++ install.sh
@@ -2,14 +2,12 @@
 
 TARGETPATH="/usr/local/bin"
 
-echo "Installing /usr/local/bin/aptdate..."
-sudo rm "$TARGETPATH/aptdate" > /dev/null 2>&1
-sudo ln -s "$(pwd)/aptdate.sh" "$TARGETPATH/aptdate"
-sudo chmod +x "$TARGETPATH/aptdate"
-
-echo "Installing /usr/local/bin/pipdate..."
-sudo rm "$TARGETPATH/pipdate" > /dev/null 2>&1
-sudo ln -s "$(pwd)/pipdate.sh" "$TARGETPATH/pipdate"
-sudo chmod +x "$TARGETPATH/pipdate"
+for toolName in "aptdate" "pipdate" "fix-newline"
+do
+  echo "Installing /usr/local/bin/$toolName..."
+  sudo rm "$TARGETPATH/$toolName" > /dev/null 2>&1
+  sudo ln -s "$(pwd)/$toolName.sh" "$TARGETPATH/$toolName"
+  sudo chmod +x "$TARGETPATH/$toolName"
+done
 
 echo "Done :)"
